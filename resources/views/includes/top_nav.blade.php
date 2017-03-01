051f5cc7cc7b76f05f5a315ca70ed46cf4ecd272
robertmcbathin <mercile55@yandex.ru>
2017-03-01 17:03:27 +0300

    INIT

---
+++ resources/views/includes/top_nav.blade.php
@@ -0,0 +1,134 @@
+<nav class="navbar navbar-default navbar-transparent navbar-fixed-top navbar-color-on-scroll" id="sectionsNav">
+    	<div class="container">
+        	<!-- Brand and toggle get grouped for better mobile display -->
+        	<div class="navbar-header">
+        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
+            		<span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+        		</button>
+        		<a class="navbar-brand" href=""><img src="/images/logo.png" height="45px" alt=""></a>
+        	</div>
+
+        	<div class="collapse navbar-collapse" id="navigation-example">
+        		<ul class="nav navbar-nav navbar-right">
+    				<li>
+						<a href="index.html">
+							<i class="material-icons">apps</i> Components
+						</a>
+					</li>
+
+					<li class="dropdown">
+						<a href="index.html#" class="dropdown-toggle" data-toggle="dropdown">
+							<i class="material-icons">view_day</i> Sections
+							<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu dropdown-with-icons">
+							<li>
+								<a href="sections.html#headers">
+									<i class="material-icons">dns</i> Headers
+								</a>
+							</li>
+							<li>
+								<a href="sections.html#features">
+									<i class="material-icons">build</i> Features
+								</a>
+							</li>
+							<li>
+								<a href="sections.html#blogs">
+									<i class="material-icons">list</i> Blogs
+								</a>
+							</li>
+							<li>
+								<a href="sections.html#teams">
+									<i class="material-icons">people</i> Teams
+								</a>
+							</li>
+							<li>
+								<a href="sections.html#projects">
+									<i class="material-icons">assignment</i> Projects
+								</a>
+							</li>
+							<li>
+								<a href="sections.html#pricing">
+									<i class="material-icons">monetization_on</i> Pricing
+								</a>
+							</li>
+							<li>
+								<a href="sections.html#testimonials">
+									<i class="material-icons">chat</i> Testimonials
+								</a>
+							</li>
+							<li>
+								<a href="sections.html#contactus">
+									<i class="material-icons">call</i> Contacts
+								</a>
+							</li>
+
+						</ul>
+					</li>
+
+					<li class="dropdown">
+						<a href="index.html#" class="dropdown-toggle" data-toggle="dropdown">
+							<i class="material-icons">view_carousel</i> Examples
+							<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu dropdown-with-icons">
+							<li>
+								<a href="examples/about-us.html">
+									<i class="material-icons">account_balance</i> About Us
+								</a>
+							</li>
+							<li>
+								<a href="examples/blog-post.html">
+									<i class="material-icons">art_track</i> Blog Post
+								</a>
+							</li>
+							<li>
+								<a href="examples/blog-posts.html">
+									<i class="material-icons">view_quilt</i> Blog Posts
+								</a>
+							</li>
+							<li>
+								<a href="examples/contact-us.html">
+									<i class="material-icons">location_on</i> Contact Us
+								</a>
+							</li>
+							<li>
+								<a href="examples/landing-page.html">
+									<i class="material-icons">view_day</i> Landing Page
+								</a>
+							</li>
+							<li>
+								<a href="examples/login-page.html">
+									<i class="material-icons">fingerprint</i> Login Page
+								</a>
+							</li>
+							<li>
+								<a href="examples/pricing.html">
+									<i class="material-icons">attach_money</i> Pricing Page
+								</a>
+							</li>
+							<li>
+								<a href="examples/product-page.html">
+									<i class="material-icons">beach_access</i> Product Page
+								</a>
+							</li>
+							<li>
+								<a href="examples/profile-page.html">
+									<i class="material-icons">account_circle</i> Profile Page
+								</a>
+							</li>
+							<li>
+								<a href="examples/signup-page.html">
+									<i class="material-icons">person_add</i> Signup Page
+								</a>
+							</li>
+						</ul>
+					</li>
+
+        		</ul>
+        	</div>
+    	</div>
+    </nav>
