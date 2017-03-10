9193b4860a944e70afdcaf66711032468b7cac07
robertmcbathin <mercile55@yandex.ru>
2017-03-10 12:01:56 +0300

    some soap fucking problems

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -23,49 +23,30 @@ class PaymentController extends Controller
     	$secHeader = new \SoapHeader($nameSpace, 'Security', $secHeaderValue);*/
  $xml_request =    
     "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://umarsh.ru/sdp/servicepojo\">
-
 <soapenv:Header>
-
 <wsse:Security soapenv:mustUnderstand=\"0\" xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\">
-
 <wsse:UsernameToken xmlns:wsu=\"...\">
-
 <wsse:Username>admin</wsse:Username>
-
 <wsse:Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText\">1</wsse:Password>
-
 </wsse:UsernameToken>
-
 </wsse:Security>
-
 </soapenv:Header>
-
 <soapenv:Body>
-
 <ser:CardInfoRequest>
-
 <ser:agentId>0036</ser:agentId>
-
 <ser:salepointId>0036</ser:salepointId>
-
 <ser:version>1</ser:version>
-
 <ser:sysNum>023333092</ser:sysNum>
-
 <ser:regionId>21</ser:regionId>
-
 <ser:deviceID>B0000001</ser:deviceID>
-
 </ser:CardInfoRequest>
-
 </soapenv:Body>
-
 </soapenv:Envelope>";
 $soap_request = new Balance(NULL, array('location' => "http://umarsh.ru/sdp/servicepojo",
                                      'uri'      => "http://umarsh.ru/sdp/servicepojo"));
 $request = $soap_request->__doRequest($xml_request, "http://umarsh.ru/sdp/servicepojo", NULL, NULL, 0);
       return view('pages.balance', [
-      	'request' => $request
+      	'request' => $xml_request
       	]);
 
 }
