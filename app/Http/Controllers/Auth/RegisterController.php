71f107ad649139243c20a9e091874631aad25d33
robertmcbathin <mercile55@yandex.ru>
2017-04-27 12:17:02 +0300

    registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use Session;
+use Mail;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -72,5 +74,49 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request,[
+            'card_number' => 'required|min:9|max:9',
+            'email' => 'required|max:255',
+            'password' => 'required|min:6|max:150',
+            'password_repeat' => 'required|min:6|max:150'
+            ]);
+        /**
+         * INITIALIZE VARIABLES
+         */
+        $card_number     = $request['card_number'];
+        $email           = $request['email'];
+        $password        = $request['password'];
+        $password_repeat = $request['password_repeat'];
+        /**
+         * CHECK PASSWORDS AND INSERT DATA
+         */
+        if ($password_repeat == $password){
+            $user = new \App\User;
+            $user->card_number = $card_number;
+            $user->username = $card_number;
+            $user->name = $card_number;
+            $user->email = $email;
+            $user->password = bcrypt($password);
+            if ($user->save()){
+              Mail::send('emails.registration',
+                ['name' => $name,
+                'email' => $email,
+                'content' => $content],
+              function ($m){
+                $m->from('activation@etk-club.ru', 'ETK21.RU');
+                $m->to('questions@etk21.ru')->subject('Новый вопрос с сайта');
+            });
+            }
+            Session::flash('register-ok', 'Спасибо за активацию аккаунта!');
+        } else Session::flash('register-fail', 'К сожалению, при сохранении данных произошла ошибка.');
+        return redirect()->back();
+        /**
+         * SENT MAIL
+         */
+        
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,139 @@
-@extends('layouts.app')
+@extends('layouts.login')
+@section('description')
 
+@endsection
+@section('keywords')
+регистрация карты етк
+@endsection
+@section('title')
+Регистрация карты
+@endsection
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+<div class="page-header header-filter login-page-header" data-parallax="active" style="background-image: url(&quot;/images/bgs/bg_index_tr.jpg&quot;); transform: translate3d(0px, 0px, 0px);">
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+
+
+                <div class="card card-signup">
+                    <h2 class="card-title text-center">Регистрация карты</h2>
+                    <div class="row">
+                    <div class="col-md-3 col-md-offset-1">
+                            <div class="info info-horizontal">
+                                <div class="icon icon-rose">
+                                    <i class="material-icons">group</i>
+                                </div>
+                                <div class="description">
+                                    <h4 class="info-title">Личный кабинет</h4>
+                                    <p class="description">
+                                        Личный кабинет держателя карты позволит Вам пользоваться услугами безналичной оплаты проезда еще удобнее
+                                    </p>
+                                </div>
                             </div>
+
                         </div>
+                        <div class="col-md-7">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                            {{ csrf_field() }}
+                                <div class="card-content">
+                                    <div class="form-group{{ $errors->has('card_number') ? ' has-error' : '' }}">
+                                        <label for="email" class="col-md-4 control-label">Номер карты</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                        <div class="col-md-6">
+                                            <input id="card_number" type="text" class="form-control" name="card_number" value="{{ old('card_number') }}" required autofocus placeholder="000000000" minlength="9" maxlength="9">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                                            @if ($errors->has('card_number'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('card_number') }}</strong>
+                                            </span>
+                                            @endif
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                        <label for="password" class="col-md-4 control-label">E-mail</label>
+
+                                        <div class="col-md-6">
+                                            <input id="email" type="email" class="form-control" name="email" required placeholder='example@mail.com'>
+                                            <p class="text-muted">На адрес электронной почты будет отправлено письмо с инструкцией по активации карты</p>
+                                            @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    
+                                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                            <label for="password" class="col-md-4 control-label">Пароль</label>
+
+                                            <div class="col-md-6">
+                                                <input id="password" type="password" class="form-control" name="password" required minlength="6" maxlength="40">
+                                                <p class="text-muted">Не менее 6 символов</p>
+                                                @if ($errors->has('password'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                     <div class="form-group{{ $errors->has('password_repeat') ? ' has-error' : '' }}">
+                                            <label for="password_repeat" class="col-md-4 control-label">Повторите пароль</label>
+
+                                            <div class="col-md-6">
+                                                <input id="password_repeat" type="password" class="form-control" name="password_repeat" required minlength="6" maxlength="40">
+
+                                                @if ($errors->has('password_repeat'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('password_repeat') }}</strong>
+                                                </span>
+                                                @endif
+                                            </div>
+                                     </div>
+
+                                    <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-4">
+                                                <div class="checkbox">
+                                                    <label>
+                                                      <input type="checkbox" name="acception" checked="checked">
+                                            Я ознакомлен(а) с <a href="#something">политикой конфиденциальности</a> и даю свое <a href="">согласие на обработку персональных данных</a>.
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="form-group">
+                                            <div class="col-md-8 col-md-offset-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    Зарегистрировать карту
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                </div>
+                                
+                            </form>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
+
+
+
             </div>
         </div>
     </div>
+    @include('includes.login-footer')
 </div>
 @endsection
+
+
+
+
+
