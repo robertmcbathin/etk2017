b8f0a5d81d2e6d402871a9c2bb6b5161c058ce16
robertmcbathin <mercile55@yandex.ru>
2017-05-02 06:08:12 +0300

    register debugging 2

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Auth;
 use DB;
+use Session;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
