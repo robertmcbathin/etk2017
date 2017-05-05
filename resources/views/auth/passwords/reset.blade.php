197e012ee372ae8438e083d7e930d3c9ab0531b2
robertmcbathin <mercile55@yandex.ru>
2017-05-05 09:35:13 +0300

    password reset form

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -79,3 +79,90 @@
     @include('includes.login-footer')
 </div>
 @endsection
+@extends('layouts.login')
+
+@section('content')
+<div class="page-header header-filter login-page-header" data-parallax="active" style="background-image: url(&quot;/images/bgs/bg_index_tr.jpg&quot;); transform: translate3d(0px, 0px, 0px);">
+    <div class="container">
+        <div class="row">
+
+
+            <div class="col-md-8 col-md-offset-2">
+
+                <div class="card card-signup">
+                    <h2 class="card-title text-center">Создать новый пароль</h2>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                             @if (session('status'))
+                             <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                            @endif
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.request') }}">
+                        {{ csrf_field() }}
+
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Пароль</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            <label for="password-confirm" class="col-md-4 control-label">Повторите пароль</label>
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Восстановить пароль
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</div>
+@endsection
