013448a562e5641c00226f0802723f1634fce0c4
robertmcbathin <mercile55@yandex.ru>
2017-07-12 10:56:03 +0300

    schedule for 1035

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,47 +26,46 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-          $schedule->call(function(){
+      $schedule->call(function(){
 
           $sources = [1,2];
           foreach ($sources as $source) {
-                
-            }  
-          $filename = '/admin/files/statuscard/statuscard-21-' . date('ymd') . '-0000' . $source . '.txt';
-          $short_filename = 'statuscard-21-' . date('ymd') . '-0000' . $source . '.txt';
-          $path = public_path() . '/admin/files/statuscard/' . $short_filename;
-          $fp = fopen($path, 'w');
-      
-          $status_count = 0;
-          if ($cards = DB::table('ETK_BLOCKLISTS')
-                      ->where('source', $source)
-                      ->where('is_loaded', 0)
-                      ->get()){
+           $filename = '/admin/files/statuscard/statuscard-21-' . date('ymd') . '-0000' . $source . '.txt';
+           $short_filename = 'statuscard-21-' . date('ymd') . '-0000' . $source . '.txt';
+           $path = public_path() . '/admin/files/statuscard/' . $short_filename;
+           $fp = fopen($path, 'w');
+
+           $status_count = 0;
+           if ($cards = DB::table('ETK_BLOCKLISTS')
+              ->where('source', $source)
+              ->where('is_loaded', 0)
+              ->get()){
             foreach ($cards as $card) {
               $status_count++;
               $row = $card->chip . "\t" . $card->operation_type . "\r\n";
               fwrite($fp, $row);
-            };
-            fclose($fp);
-            if (DB::table('ETK_STATUSCARDS')
-                ->insert(['filename' => $filename,
-                          'status_count' => $status_count,
-                          'created_by' => 0])){
+          };
+          fclose($fp);
+          if (DB::table('ETK_STATUSCARDS')
+            ->insert(['filename' => $filename,
+              'status_count' => $status_count,
+              'created_by' => 0])){
             DB::table('ETK_BLOCKLISTS')
-              ->where('source', $source)
-              ->where('is_loaded', 0)
-              ->update(['is_loaded' => 1]);
+               ->where('source', $source)
+               ->where('is_loaded', 0)
+               ->update(['is_loaded' => 1]);
             /**
              * LOAD FILE TO FTP
              */
             $ftp_file = fopen($path, 'r');
             Storage::disk('ftp-cott-blocklists')->put($short_filename, $ftp_file);
             fclose($ftp_file);
-            }
-          }
-    })->everyMinute(); 
+        }
+    } 
+}  
+})->everyMinute(); 
 
-    }
+  }
 
     /**
      * Register the Closure based commands for the application.
