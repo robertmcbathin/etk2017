45e7f0edc2d9ac15db3c35e6d6f7f745a9eba299
m55 <m55@localhost.localdomain>
2017-09-11 19:17:38 +0300

    0.9.1 get parameters troubles

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -236,8 +236,7 @@ public function getNewsPage(){
          * @var [type]
          */
         if ($order->rewrite_status == 1){
-          Session::flash('error','Отложенное пополнение по этому заказу уже создано');
-          return view('pages.profile.test.payment.payment_ok');
+          return;
         } elseif ($order->rewrite_status == 0) {
         /**
          * POST TRANSACTION
@@ -266,17 +265,15 @@ public function getNewsPage(){
               'rewrite_status' => 1
               ]);
         } catch (Exception $e) {
-          return view('pages.profile.test.payment.payment_fail');
+          return;
         }
-        Session::flash('success','Операция прошла успешно!');
-        return view('pages.profile.test.payment.payment_ok');
+        return;
         }
       } else{
         /**
          * ОПЛАТА НЕ ПРОИЗВЕДЕНА
          */
-        Session::flash('warning','На данный момент, оплата еще не прошла. Как только мы получим уведомление от Банка, Вам придет письмо на элктронную почту.');
-        return view('pages.profile.test.payment.payment_fail');
+        return;
       }
 
 
