8679f9255f0c52c2fb9555de651611d888822ba8
M55 <m55@localhost.localdomain>
2018-02-26 15:05:37 +0300

    messages

--- resources/views/pages/profile/messages.blade.php
+++ resources/views/pages/profile/messages.blade.php
@@ -49,45 +49,115 @@
         @endif
       <div class="row">
         <div class="col-md-8 col-md-offset-2">
-          <h4 class="title">Мои сообщения</h4>
+          <h4 class="title">Участие в обсуждениях</h4>
           @if(count($messages) > 0)
-        <h3 class="title text-center">Комментарии: {{ count($messages) }}</h3>
-        @foreach($messages as $message)
-        @if($message->reply_to == null)
-        <div class="media">
-          <a class="pull-left" href="#">
-              <div class="avatar">
-                <img class="media-object" alt="64x64" src="http://etk21.ru{{ $message->profile_image }}">
-            </div>
-        </a>
-        <div class="media-body">
-            @isset($message->post) <h6>{{ $message->post }}</h6> @endisset
-            <h4 class="media-heading">{{ $message->name }} {{ $message->lastname }} <small>· {{ $message->created_at }} </small></h4>
+    <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">#</th>
+                                        <th>Name</th>
+                                        <th>Job Position</th>
+                                        <th>Since</th>
+                                        <th class="text-right">Salary</th>
+                                        <th class="text-right">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-center">1</td>
+                                        <td>Andrew Mike</td>
+                                        <td>Develop</td>
+                                        <td>2013</td>
+                                        <td class="text-right">€ 99,225</td>
+                                        <td class="td-actions text-right">
+                                            <button type="button" rel="tooltip" class="btn btn-info" data-original-title="" title="">
+                                                <i class="material-icons">person</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-success" data-original-title="" title="">
+                                                <i class="material-icons">edit</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-danger" data-original-title="" title="">
+                                                <i class="material-icons">close</i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
 
-
-            <p>{{ $message->text }}</p>
-
-                @foreach($messages as $reply) 
-                @if($reply->reply_to == $message->id) 
-                <div class="media">
-                  <a class="pull-left" href="#">
-                    <div class="avatar">
-                        <img class="media-object" alt="64x64" src="http://etk21.ru{{ $reply->profile_image }}">
-                    </div>
-                </a>
-                <div class="media-body">
-                    @isset($reply->post) <h6>{{ $reply->post }}</h6> @endisset
-                    <h4 class="media-heading">{{ $reply->name }} {{ $reply->lastname }}<small>· {{ $reply->created_at }}</small></h4>
-                    <p>{{ $reply->text }}</p>
-
-                </div>
-            </div>
-            @endif
-            @endforeach
-        </div>
-    </div>
-    @endif
-    @endforeach
+                                        <td class="text-center">2</td>
+                                        <td>John Doe</td>
+                                        <td>Design</td>
+                                        <td>2012</td>
+                                        <td class="text-right">€ 89,241</td>
+                                        <td class="td-actions text-right">
+                                            <button type="button" rel="tooltip" class="btn btn-info btn-round" data-original-title="" title="">
+                                                <i class="material-icons">person</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-success btn-round" data-original-title="" title="">
+                                                <i class="material-icons">edit</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-danger btn-round" data-original-title="" title="">
+                                                <i class="material-icons">close</i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-center">3</td>
+                                        <td>Alex Mike</td>
+                                        <td>Design</td>
+                                        <td>2010</td>
+                                        <td class="text-right">€ 92,144</td>
+                                        <td class="td-actions text-right">
+                                            <button type="button" rel="tooltip" class="btn btn-info btn-simple" data-original-title="" title="">
+                                                <i class="material-icons">person</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-success btn-simple" data-original-title="" title="">
+                                                <i class="material-icons">edit</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-danger btn-simple" data-original-title="" title="">
+                                                <i class="material-icons">close</i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-center">4</td>
+                                        <td>Mike Monday</td>
+                                        <td>Marketing</td>
+                                        <td>2013</td>
+                                        <td class="text-right">€ 49,990</td>
+                                        <td class="td-actions text-right">
+                                            <button type="button" rel="tooltip" class="btn btn-info btn-round" data-original-title="" title="">
+                                                <i class="material-icons">person</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-success btn-round" data-original-title="" title="">
+                                                <i class="material-icons">edit</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-danger btn-round" data-original-title="" title="">
+                                                <i class="material-icons">close</i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-center">5</td>
+                                        <td>Paul Dickens</td>
+                                        <td>Communication</td>
+                                        <td>2015</td>
+                                        <td class="text-right">€ 69,201</td>
+                                        <td class="td-actions text-right">
+                                            <button type="button" rel="tooltip" class="btn btn-info" data-original-title="" title="">
+                                                <i class="material-icons">person</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-success" data-original-title="" title="">
+                                                <i class="material-icons">edit</i>
+                                            </button>
+                                            <button type="button" rel="tooltip" class="btn btn-danger" data-original-title="" title="">
+                                                <i class="material-icons">close</i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </div>
           @else
             <p>У Вас пока нет сообщений. Вы можете оставить комментарий под интересующей Вас новостью.</p>
           @endif
